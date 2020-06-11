96380f1c74514acf0458d05c0c07100772bd1ddf
Sadegh <Sadegh_Meratnia@outlook.com>
2020-06-11 12:30:38 +0430

    initialize the project

---
+++ SwiftUIDemoWithGithubAPIs/SwiftUIDemoWithGithubAPIs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoWithGithubAPIs
+//
+//  Created by Sadegh on 3/22/1399 AP.
+//  Copyright © 1399 Sadegh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
